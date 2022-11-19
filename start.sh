63b0d42bf8eeb07035b2957c5d5fbf8851066871
sepulzera <hartung_spam@gmx.de>
2022-11-19 11:33:10 +0100

    Fix env-var ADMIN_URL not being recognized
    See https://github.com/ownrecipes/OwnRecipes/issues/78

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@
 # Sed replace the API_PORT vars.
 
 sed -i "s/API_PORT/$API_PORT/g" /etc/nginx/conf.d/default.conf;
+sed -i "s/ADMIN_URL/${ADMIN_URL:-admin}/g" /etc/nginx/conf.d/default.conf;
 
 # Start the NGINX service.
 
